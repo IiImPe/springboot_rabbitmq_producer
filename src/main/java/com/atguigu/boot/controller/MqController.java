2c41c9f6354920a903ecb81e99bcb7dc9a32f8b6
IiImPe <331493305@qq.com>
2022-07-07 14:34:04 +0800

    4th commit

--- src/main/java/com/atguigu/boot/controller/MqController.java
+++ src/main/java/com/atguigu/boot/controller/MqController.java
@@ -11,6 +11,7 @@ public class MqController {
     @RabbitListener(queues = "boot_queue01")
     public void consume(Message message) {
         System.out.println("lll");
+        System.out.println("github test");
         System.out.println("得得得得是独立开发经理");
         System.out.println("消息本身的内容: " + new String(message.getBody()));
     }
